93ebf944c7dc567c70d72b49ee6153a58189ab6b
gpsnmeajp <33391403+gpsnmeajp@users.noreply.github.com>
2020-05-02 21:17:46 +0900

    jsonから設定を読み込む機能を追加

--- VMC-Websocket-OBS/Main.cs
+++ VMC-Websocket-OBS/Main.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.IO;
+using System.Text;
 using System.Threading;
 using System.Collections.Generic;
 using System.Linq;
@@ -6,19 +8,17 @@ using System.Text;
 using System.Threading.Tasks;
 
 using Rug.Osc;
+using Newtonsoft.Json;
 
 namespace VMC_Websocket_OBS
 {
     class Main
     {
-        const int port = 39539;
-        const string adr = "ws://127.0.0.1:4444";
-        const string pass = "";
+        Setting setting = null; //設定
 
         OBS obs = new OBS();
         VMC vmc = new VMC();
 
-        bool exit = false;
         int old_calibration_state = -1;
 
         //起動～終了処理
@@ -28,18 +28,9 @@ namespace VMC_Websocket_OBS
 
             try
             {
-                Console.WriteLine("# Connecting to OBS Websocket...");
-                //OBS-Websocket切断時コールバックを設定
-                obs.OnDisconnected = Shutdown;
-                obs.OnOBSExit = Shutdown;
-                //OBS-Websocketに接続
-                obs.Start(adr, pass);
+                LoadJson();
 
-                Console.WriteLine("# Start standby for VMC Protocol");
-                //メッセージ受信コールバックを設定
-                vmc.OnMessage = OnMessage;
-                //VMCの通信を待ち受け
-                vmc.Start(port);
+                Connect();
 
                 //ユーザーの終了操作を待機
                 Console.WriteLine("### Press ENTER key to EXIT");
@@ -50,26 +41,54 @@ namespace VMC_Websocket_OBS
                 Console.WriteLine("# MainThread : "+e);
             }
 
-            Console.WriteLine("### EXIT...");
+            Console.WriteLine("### EXIT");
+            Disconnect();
+        }
 
-            //OBS-Websocket切断時コールバックを解除
+        //JSON設定ファイルを読み込む
+        private void LoadJson()
+        {
+            string json = File.ReadAllText("setting.json", new UTF8Encoding(false));
+            setting = JsonConvert.DeserializeObject<Setting>(json);
+
+            Console.WriteLine("# Setting loaded");
+        }
+
+        //接続開始処理
+        private void Connect()
+        {
+            Console.WriteLine("# Connecting to OBS Websocket...");
+            //OBS-Websocket切断時コールバック(接続失敗・異常切断)を設定
+            obs.OnDisconnected = Shutdown;
+            obs.OnOBSExit = Shutdown;
+            //OBS-Websocketに接続
+            obs.Start(setting.OBSWebSocketURI, setting.OBSWebSocketPassword);
+
+            Console.WriteLine("# Start standby for VMC Protocol");
+            //メッセージ受信コールバックを設定
+            vmc.OnMessage = OnMessage;
+            //VMCの通信を待ち受け
+            vmc.Start(setting.VMCProtocolPort);
+        }
+        //切断処理
+        private void Disconnect()
+        {
+            //OBS-Websocket切断時コールバック(接続失敗・異常切断)を解除
             obs.OnDisconnected = null;
             obs.OnOBSExit = null;
 
             vmc.Stop();
             obs.Stop();
-            Console.WriteLine("### OK");
         }
 
         //通信失敗、OBSの終了などの通知を受けたとき
         private void Shutdown()
         {
             Console.WriteLine("### Couldn't connect to OBS");
-            //強制的に終了する
             try
             {
-                vmc.Stop();
-                obs.Stop();
+                //強制的に切断する
+                Disconnect();
             }
             catch (Exception e) {
                 Console.WriteLine("# Callback : " + e);
@@ -77,13 +96,16 @@ namespace VMC_Websocket_OBS
             System.Environment.Exit(-1);
         }
 
-        //VMCからのMessageイベント
+        //VMCからのMessageを処理する
         private void OnMessage(OscMessage message)
         {
+            // VMC Protocolを参照
+            // https://sh-akira.github.io/VirtualMotionCaptureProtocol/
             if (message.Address == "/VMC/Ext/OK")
             {
                 if (message.Count >= 3 && message[1] is int)
                 {
+                    //キャリブレーション状態を取り出し
                     int calibration_state = (int)message[1];
                     if (old_calibration_state != calibration_state)
                     {
@@ -91,13 +113,11 @@ namespace VMC_Websocket_OBS
                         old_calibration_state = calibration_state;
                         if (calibration_state == 3)
                         {
-                            Console.WriteLine("Scene1");
-                            obs.SetScene("Scene1");
+                            obs.SetScene(setting.SceneOfCalibrationComplete);
                         }
                         else
                         {
-                            Console.WriteLine("Scene2");
-                            obs.SetScene("Scene2");
+                            obs.SetScene(setting.SceneOfCalibrationInProgress);
                         }
                     }
                 }
---
+++ VMC-Websocket-OBS/Setting.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VMC_Websocket_OBS
+{
+    class Setting
+    {
+        public int VMCProtocolPort { set; get; }
+        public string OBSWebSocketURI { set; get; }
+        public string OBSWebSocketPassword { set; get; }
+
+
+        public string SceneOfCalibrationComplete { set; get; }
+        public string SceneOfCalibrationInProgress { set; get; }
+    }
+}
